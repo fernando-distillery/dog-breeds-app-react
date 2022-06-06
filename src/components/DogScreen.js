b2e5692800c7cd137b8118730fad81b56ea850c4
Fernando Martínez <fernando.martinez@distillery.com>
2022-06-06 13:22:28 -0500

    Shows a dog image after clicking in the available list.

--- src/components/DogScreen.js
+++ src/components/DogScreen.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { getBreedRandomImage } from "../helpers/helpers";
 
 export const DogScreen = () => {
+  const [breedRandomImage, setBreedRandomImage] = useState("");
   const { breedName } = useParams();
+
+  const getBreedRandomImageUrl = async (breedName) => {
+    const imageUrl = await getBreedRandomImage(breedName);
+    setBreedRandomImage(imageUrl);
+  };
+
+  useEffect(() => {
+    getBreedRandomImageUrl(breedName);
+  }, [breedName]);
 
   return (
     <div>
       <h1>{breedName}</h1>
+      {breedRandomImage && breedName && (
+        <img src={breedRandomImage} alt={`A dog whose breed is ${breedName}`} />
+      )}
     </div>
   );
 };
