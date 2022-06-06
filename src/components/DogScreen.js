6af80d6dae63d33e43243f5cd8105307a338a5a4
Fernando Martínez <fernando.martinez@distillery.com>
2022-06-06 14:14:16 -0500

    Adds styling on DogScreen and implements a back button.

--- src/components/DogScreen.js
+++ src/components/DogScreen.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { getBreedRandomImage } from "../helpers/helpers";
+import "../styles.css";
 
 export const DogScreen = () => {
   const [breedRandomImage, setBreedRandomImage] = useState("");
   const { breedName } = useParams();
+  const navigate = useNavigate();
 
   const getBreedRandomImageUrl = async (breedName) => {
     const imageUrl = await getBreedRandomImage(breedName);
@@ -16,11 +18,29 @@ export const DogScreen = () => {
   }, [breedName]);
 
   return (
-    <div>
-      <h1>{breedName}</h1>
+    <div className="dog-screen__container">
+      <h1 className="breedname">
+        {breedName.substring(0, 1).toUpperCase() + breedName.substring(1)}
+      </h1>
       {breedRandomImage && breedName && (
-        <img src={breedRandomImage} alt={`A dog whose breed is ${breedName}`} />
+        <img
+          className="dog__img"
+          src={breedRandomImage}
+          alt={`A dog whose breed is ${breedName}`}
+        />
       )}
+      <button
+        className="back-button"
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        <i
+          className="fa fa-arrow-left"
+          aria-hidden="true"
+          style={{ fontSize: "24px" }}
+        ></i>
+      </button>
     </div>
   );
 };
