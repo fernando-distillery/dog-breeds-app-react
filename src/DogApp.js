b6f7c3fb5a1c6f4baf59edd73f16bf8e664201de
Fernando Martínez <fernando.martinez@distillery.com>
2022-06-08 15:22:43 -0500

    Adds dark mode functionality using Context API.

--- src/DogApp.js
+++ src/DogApp.js
@@ -1,10 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 import { DogFooter } from "./components/DogFooter";
+import { ThemeContext } from "./contexts/ThemeContext";
 import { getBreeds } from "./helpers/helpers";
 
 export const DogApp = () => {
   const navigate = useNavigate();
+  const { darkMode, toggleDarkMode } = useContext(ThemeContext);
 
   const [breeds, setBreeds] = useState([]);
   const [year, setYear] = useState();
@@ -23,7 +25,21 @@ export const DogApp = () => {
   }, []);
 
   return (
-    <main>
+    <main className={`${darkMode && "dark"}`}>
+      <button
+        onClick={toggleDarkMode}
+        style={{
+          position: "relative",
+          top: "24px",
+          left: "88%",
+          width: "3em",
+          height: "3em",
+          border: "none",
+          backgroundColor: "gray",
+        }}
+      >
+        <i className="fas fa-moon"></i>
+      </button>
       <Outlet context={[breeds]} />
       <DogFooter year={year} />
     </main>
--- src/index.js
+++ src/index.js
@@ -6,25 +6,28 @@ import { DogApp } from "./DogApp";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { DogScreen } from "./components/DogScreen";
 import { DogMain } from "./components/DogMain";
+import { ThemeProvider } from "./contexts/ThemeContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<DogApp />}>
-        <Route path="main" element={<DogMain />} />
-        <Route path="breeds" element={<DogScreen />}>
-          <Route path=":breedName" element={<DogScreen />} />
+  <ThemeProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<DogApp />}>
+          <Route path="main" element={<DogMain />} />
+          <Route path="breeds" element={<DogScreen />}>
+            <Route path=":breedName" element={<DogScreen />} />
+          </Route>
         </Route>
-      </Route>
-      <Route
-        path="*"
-        element={
-          <main style={{ padding: "1rem" }}>
-            <p>There's nothing here!</p>
-          </main>
-        }
-      />
-    </Routes>
-  </BrowserRouter>
+        <Route
+          path="*"
+          element={
+            <main style={{ padding: "1rem" }}>
+              <p>There's nothing here!</p>
+            </main>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  </ThemeProvider>
 );
