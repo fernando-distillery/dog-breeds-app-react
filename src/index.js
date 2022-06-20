ef3f4a897cde3f6604b56667f0f0fa0584106e3f
Fernando Martínez <fernando.martinez@distillery.com>
2022-06-20 15:01:54 -0500

    Sets hashtype to "slash" on HashRouter.

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { ThemeProvider } from "./contexts/ThemeContext";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ThemeProvider>
-    <HashRouter>
+    <HashRouter hashType={"slash"}>
       <Routes>
         <Route path="/" element={<DogApp />}>
           <Route path="main" element={<DogMain />} />
